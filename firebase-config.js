4271c919db5393f12948cd2b90a5b73531a87b45
yusufkagann <9kaanv4@gmail.com>
2026-03-29 18:22:53 +0300

    uygulamada güncellemeler yapıldı

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyCgEVPqtQqDTzao1a2LgAL4yW6jk26JjNA",
     authDomain: "quizapp-2327e.firebaseapp.com",
     projectId: "quizapp-2327e",
     storageBucket: "quizapp-2327e.firebasestorage.app",
     messagingSenderId: "573771860460",
-    appId: "1:573771860460:web:8289ea3fddebf097ce98c9", // BURAYA VİRGÜL EKLEDİM
-    databaseURL: "https://quizapp-2327e-default-rtdb.firebaseio.com"
+    appId: "1:573771860460:web:8289ea3fddebf097ce98c9",
+    databaseURL: "https://quizapp-2327e-default-rtdb.europe-west1.firebasedatabase.app"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export { app }; // Eğer diğer dosyalar bu config'i kullanıyorsa bu satır önemli
+// V9/10 Compat (Eski tarz) başlatma
+// Sizin uygulamanızda NPM/Server olmadığı için import/export KULLANILMAZ 
+firebase.initializeApp(firebaseConfig);
+const db = firebase.database();
